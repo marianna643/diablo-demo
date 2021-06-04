e3af69e7684748bba51114ef30f5f301ee38a64c
marianna643 <trma0520@gmail.com>
2021-06-05 01:39:57 +0200

    titel settings homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 
+
 function HomePage() {
     return (
-        <div  className="background-container">
+        <div className="background-container">
+            <title className="titel-1">Diablo Studio</title>
+            <title className="titel-2">Demo Drop</title>
         </div>
-    );
+);
 }
 
 export default HomePage;
