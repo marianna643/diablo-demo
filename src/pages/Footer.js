13521307d3bfc4e12822aae5255d8fbb314e13a9
marianna643 <trma0520@gmail.com>
2021-06-06 22:17:40 +0200

    sign button

---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+    <div className="footer">
+        <div className="container">
+        <button type="button" className="sign">
+            Sign in
+        </button>
+        <button type="button" className="sign">
+            Sign up
+        </button>
+        </div>
+    </div>
+);
+
+export default Footer;
