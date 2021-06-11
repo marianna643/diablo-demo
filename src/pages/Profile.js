da3a06e54aa29f5e7ff1c0401e995b914de46505
marianna643 <trma0520@gmail.com>
2021-06-11 23:30:07 +0200

    profile page style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import TopMenu from '../components/TopMenu/TopMenu';
+import './Profile.css';
 
 
 function Profile () {
     return (
         <>
             <TopMenu/>
+            <div className="profile">
+                <form>
+                    <h1>My profile</h1>
+                        <div className="formProfile-group">
+                        <label htmlFor="username">User Name *</label>
+                        <input type="text" name="username" id="username"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="firstname">First Name *</label>
+                        <input type="text" name="firstname" id="firstname"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="lastname">Last Name *</label>
+                        <input type="text" name="lastname" id="lastname"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="email">Email address *</label>
+                        <input type="email" name="email" id="email"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="country">Country *</label>
+                        <input type="text" name="country" id="country"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="country">Country *</label>
+                        <input type="text" name="country" id="country"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="facebook">Facebook</label>
+                        <input type="url" name="facebook" id="facebook"/>
+                    </div>
+                    <div className="formProfile-group">
+                        <label htmlFor="instagram">Instagram</label>
+                        <input type="url" name="instagram" id="instagram"/>
+                    </div>
+                    <button type="button" className="update-btn">
+                    Update
+                    </button>
+                </form>
+            </div>
         </>
     );
 }
