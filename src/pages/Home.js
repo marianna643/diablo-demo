d0859aacb74842f72c9e98753b0afc0c99978fa1
marianna643 <trma0520@gmail.com>
2021-06-11 22:00:26 +0200

    position for copyright text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TopMenu from '../components/TopMenu/TopMenu';
+import { Link } from 'react-router-dom';
 import demoDrop from '../assets/demodrop.jpg';
 import './Home.css';
 
@@ -16,7 +17,9 @@ function Home () {
         </div>
         <div className="text-container">
             <h2>"Drop it like it is hot"</h2>
+            <button type="button" className="demoNavigatie"  as={Link} to="/home"> I want to drop it </button>
         </div>
+        <h5>All demos are automatically submitted for the DIABLO STUDIO record label and Don Diablo’s radio show unless otherwise specified.</h5>
     </div>
 
 
