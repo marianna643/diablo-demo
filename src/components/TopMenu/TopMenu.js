f3c0ca8cabb8393d2165613a26951eaf4f057895
marianna643 <trma0520@gmail.com>
2021-07-16 00:05:58 +0200

    change topmenu, add new navlink

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,68 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './TopMenu.css';
-import { useHistory } from 'react-router-dom';
-import {AuthContext, useAuthState } from "../context/AuthContext";
-import {useContext} from "react";
+import { useContext, useState }  from 'react';
+import { useHistory } from "react-router-dom";
+import {authContext, useAuthState } from "../contexts/AuthContext";
 
 
 function TopMenu() {
 
-    const { logout } = useContext(AuthContext);
-    const { user, isAuthenticated } = useAuthState();
     const history = useHistory();
+    const { logout } = useContext(authContext);
+    const { isAuthenticated } = useAuthState();
 
 
     return (
         <nav>
             <div className="nav-container">
-                <h4>Demo Drop</h4>
                 <li>
-                    <NavLink className="home-pagina" to="/diablo" activeClassName="active-link">Demo Drop</NavLink>
+                    <NavLink className="home-pagina" to="/" activeClassName="active-link">Demo Drop</NavLink>
                 </li>
                 <ul>
-                <li>
-                <NavLink to="/profile" activeClassName="active-link">Profile</NavLink>
-                    </li>
-
-                    <li>
-                    <NavLink to="/my-demos" activeClassName="active-link">Demos</NavLink>
-                    </li>
-
-                    <li>
-                {isAuthenticated ? (
-                    <NavLink to="/sign-out" activeClassName="active-link" onClick={() =>logout()}>Sign out</NavLink>
-                    ):(
-                    history.push("/sign-out")
-                    )}
-                    </li>
-                    {user && (user.roles).includes("ROLE_ADMIN") &&
                     <li>
                         <NavLink to="/profile" activeClassName="active-link">Profile</NavLink>
                     </li>
-                    <>
-                    <li>
-                        <NavLink to="/my-demos" activeClassName="active-link">MyDemos</NavLink>
-                    </li>
-
                     <li>
                         <NavLink to="/demo-upload" activeClassName="active-link">Demo Upload</NavLink>
                     </li>
-
                     <li>
-                        {isAuthenticated ? (
-                        <NavLink to="/sign-out" activeClassName="active-link" onClick={() =>logout()}>Sign out</NavLink>
-                        ):(
-                            history.push("/sign-out")
-                        )}
+                        <NavLink to="/my-demos" activeClassName="active-link">MyDemos</NavLink>
                     </li>
-                        </>
+                    <li>
+                        <NavLink to="/all-demos" activeClassName="active-link">Review</NavLink>
+                    </li>
+                    {isAuthenticated ? (
+                        <NavLink to="/sign-out" activeClassName="active-link" onClick={() =>logout()}>Sign out</NavLink>
+                    ) : (
+                        history.push("/sign-out")
+
+                    )}
                 </ul>
+
             </div>
         </nav>
     );
 }
 
 export default TopMenu;
-
-
