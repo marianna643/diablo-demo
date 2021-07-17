cf194d3c02a90ea491c42925f0cd3eb632da6840
marianna643 <trma0520@gmail.com>
2021-07-17 21:36:31 +0200

    add validation to register

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,88 +1,80 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { useContext } from "react";
+import {Link, useHistory} from "react-router-dom";
 import axios from "axios";
 import {authContext, useAuthState} from "../components/contexts/AuthContext";
 import './LoginPage.css';
-import {Link, useHistory} from "react-router-dom";
 
-
-
-function LoginPage () {
+function Login(){
     const { login } = useContext(authContext);
     const { isAuthenticated } = useAuthState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const { handleSubmit, register } = useForm();
     console.log("AUTH STUFF:", login);
     const history = useHistory();
 
     useEffect(() => {
         if (isAuthenticated === true) {
             history.push('/');
-
         }
     }, [isAuthenticated]);
 
-    async function onSubmit(event) {
-        setLoading(true);
-        setError('');
-        event.preventDefault();
+    async function onSubmit(data){
         try {
             const response = await axios.post('http://localhost:8081/api/auth/login', {
-                username: username,
-                password: password,
+                username: data.username,
+                password: data.password,
             })
             login(response.data);
         } catch(e) {
             console.error(e);
-            setError('Login failed');
+            console.log("Inloggen is mislukt", e);
+
         }
-        setLoading(false);
     }
 
 
-
-    return (
+    return(
         <>
             <div className="background-container">
-                    <title className="titel-1">Diablo Studio</title>
-                    <title className="titel-2">Demo Drop</title>
+                <title className="titel-1">Diablo Studio</title>
+                <title className="titel-2">Demo Drop</title>
             </div>
             <div className="form-container">
-                <form className="signIn-form"onSubmit={onSubmit}>
+                <form className="signIn-form" onSubmit={handleSubmit(onSubmit)}>
                     <h3 className="popup-h3">Sign in</h3>
-                    <label className="sign-lab" htmlFor="username-field">
-                        Username: * </label>
-                    <input className="sign-input"
-                           type="text"
-                           id="username-field"
-                           value={username}
-                           onChange={(e) => setUsername(e.target.value)}>
-                    </input>
+                    <label className="sign-lab" htmlFor="email-field">
+                        Username: *
+                        <input className="sign-input"
+                               type="username"
+                               id="email-field"
+                               name="username"
+                               {...register("username")}
+                        />
+                    </label>
 
                     <label className="sign-lab" htmlFor="password-field">
-                        Password: * </label>
-                    <input className="sign-input"
-                           type="password"
-                           id="password-field"
-                           value={password}
-                           onChange={(e) => setPassword(e.target.value)}>
-                    </input>
-                    <button type="submit" className="sign-btn" >
+                        Password: *
+                        <input className="sign-input"
+                               type="password"
+                               id="password-field"
+                               name="password"
+                               {...register("password")}
+                        />
+                    </label>
+                    <button type="submit" className="sign-btn">
                         Sign in
                     </button>
-                    <p className="paragraph-form">New to Demo Drop? <Link className="signUp-link"to="/register">Create an account.</Link> </p>
+                    <p className="paragraph-form">New to Demo Drop? <Link className="signUp-link"to="/signup">Create an account.</Link> </p>
                 </form>
             </div>
-                    <div className="button-container">
-                        <Link className="termsLink" to="/terms-service"> Terms of Service </Link>
-                        {/*<p className="trade-mark">@2021 Demo Drop</p>*/}
-                    </div>
-
+            <div className="button-container">
+                <Link className="termsLink" to="/terms-service"> Terms of Service </Link>
+                {/*<p className="trade-mark">@2021 Demo Drop</p>*/}
+            </div>
         </>
     );
 }
 
-export default LoginPage;
 
+export default Login;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,44 +1,40 @@
-import React from 'react';
-import Register from "../components/Login-Register/Register";
-import {Link} from "react-router-dom";
-import './Home.css';
-import {useHistory} from "react-router-dom";
+import React,  { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './RegisterPage.css';
+import {Link, useHistory} from "react-router-dom";
+import axios from "axios";
 
 
 
-function RegisterPage () {
-
+function Register() {
     const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [firstname, setFirstname] = useState('');
-    const [country, setCountry] = useState('');
-    const [instagram, setInstagram] = useState('');
-    const [facebook, setFacebook] = useState('');
-    const [createUserSuccess, setCreateUserSuccess] = useState(false);
-    const [loading, toggleLoading] = useState(false);
+    const { handleSubmit, register ,formState: { errors }} = useForm();
+    const [success, setSucces] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
 
-    async function onSubmit(event) {
-        toggleLoading(true);
-        setError('');
 
-        event.preventDefault();
-
+    async function onSubmit(data) {
+        // console.log("DATA VAN DE GEBRUIKER??", data);
         try {
             const response = await axios.post('http://localhost:8081/api/auth/signup', {
-                username: username,
-                email: email,
-                password: password,
+                username: data.username,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                country: data.country,
+                password: data.password,
+                email: data.email,
+                facebook: data.facebook,
+                instagram: data.instagram,
                 role: ["user"],
             });
 
             if (response.status === 200) {
-                setCreateUserSuccess(true);
+                console.log(response);
+                setSucces(true);
+                setTimeout(() => history.push("/login"), 2000);
             }
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             if (e.message.includes('400')) {
                 setError('User already exists');
@@ -46,110 +42,161 @@ function RegisterPage () {
                 setError('Something went wrong while fetching data');
             }
         }
-        toggleLoading(false);
+
+        setLoading(false);
     }
 
-
     return (
-        <>  <div className="background-container">
-            <title className="titel-1">Diablo Studio</title>
-            <title className="titel-2">Demo Drop</title>
-            <div className="button-container">
-                <div className="copyright">
-                    <Link className="termsLink" to="/terms-service"> Terms of Service </Link>
-                    <p>@2021 Demo Drop</p>
+        <>
+            <p>{success && "Registeren is gelukt!"}</p>
+            <p>{loading && "Moment geduld aub"}</p>
+            <div className="background-container">
+                <title className="titel-1">Diablo Studio</title>
+                <title className="titel-2">Demo Drop</title>
+                <div className="button-container">
+                    <div className="copyright">
+                        <Link className="termsLink" to="/terms-service"> Terms of Service </Link>
+                        {/*<p>@2021 Demo Drop</p>*/}
+                    </div>
+                    <div className="form-container">
+                        {setSucces === true && <p>User is created!</p>}
+                        <form className="signUp-form" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-inner-register">
+                                <h3 className="popup-h3">Sign up</h3>
+                                <label className="sign-lab" htmlFor="username-field">
+                                    Username: *
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="username-field"
+                                           name="username"
+                                           {...register("username", {
+                                               required: true,
+                                               minLength: 3
+                                           })}
+                                    />
+                                </label>
+                                {errors?.username?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.username?.type === "minLength" && (
+                                    <p className="errorMessage">Username has to be minimum 3 character</p>)}
+
+                                <label className="sign-lab" htmlFor="firstName-field">
+                                    First name: *
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="firstName-field"
+                                           name="firstName"
+                                           {...register("firstName", {
+                                               required: true,
+                                               minLength: 3
+                                           })}
+                                    />
+                                </label>
+                                {errors?.firstName?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.firstName?.type === "minLength" && (
+                                    <p className="errorMessage">First name has to be minimum 3 character</p>)}
+
+                                <label className="sign-lab" htmlFor="lastName-field">
+                                    Last name: *
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="lastName-field"
+                                           name="lastName"
+                                           {...register("lastName", {
+                                               required: true,
+                                               minLength: 3
+                                           })}
+                                    />
+                                </label>
+                                {errors?.lastName?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.lastName?.type === "minLength" && (
+                                    <p className="errorMessage">Last name has to be minimum 3 character</p>)}
+
+                                <label className="sign-lab" htmlFor="country-field">
+                                    Country: *
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="country-field"
+                                           name="country"
+                                           {...register("country", {
+                                               required: true,
+                                               minLength: 3
+                                           })}
+                                    />
+                                </label>
+                                {errors?.country?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.country?.type === "minLength" && (
+                                    <p className="errorMessage">Country has to be minimum 3 character</p>)}
+
+
+                                <label className="sign-lab" htmlFor="email-field">
+                                    Email address: *
+                                    <input className="sign-input"
+                                           type="email"
+                                           id="email-field"
+                                           name="email"
+                                           {...register("email", {
+                                               required: true,
+                                               maxLength: 50,
+                                               pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+                                           })}
+                                    />
+                                </label>
+                                {errors?.email?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.email?.type === "pattern" &&
+                                (<p className="errorMessage">Please enter a valid email format</p>)}
+                                {errors?.email?.type === "maxLength" && (
+                                    <p className="errorMessage">Password can be maximum 50 character</p>)}
+
+
+                                <label className="sign-lab" htmlFor="password-field">
+                                    Password: *
+                                    <input className="sign-input"
+                                           type="password"
+                                           id="password-field"
+                                           name="password"
+                                           {...register("password", {
+                                               required: true,
+                                               minLength: 8
+                                           })}
+                                    />
+                                </label>
+                                {errors?.password?.type === "required" && <p className="errorMessage">This field is required</p>}
+                                {errors?.password?.type === "minLength" && (
+                                    <p className="errorMessage">Password has to be minimum 8 character</p>)}
+
+                                <label className="sign-lab" htmlFor="facebook-field">
+                                    Facebook:
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="facebook-field"
+                                           name="facebook"
+                                           {...register("facebook")}
+                                    />
+                                </label>
+
+                                <label className="sign-lab" htmlFor="instagram-field">
+                                    Instagram:
+                                    <input className="sign-input"
+                                           type="text"
+                                           id="instagram-field"
+                                           name="instagram"
+                                           {...register("instagram")}
+                                    />
+                                </label>
+
+                                <button type="submit" className="sign-btn" disabled={loading}>
+                                    {loading ? "signing up" : "Sign up"}
+                                </button>
+                                {error && <p>{error}</p>}
+                                <p className="paragraph-form">Do you have an account? <Link className="signIn-link"to="/login">Sign In</Link>{" "} </p>
+                            </div>
+                        </form>
+                        )}
+                    </div>
                 </div>
-                <div className="form-container">
-                    {createUserSuccess === true && <p>User is created!</p>}
-                    <form className="signUp-form" onSubmit={onSubmit}>
-                        <h3 className="popup-h3">Sign up</h3>
-                        <label className="sign-lab" htmlFor="username-field">
-                            Username: * </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="username-field"
-                               value={username}
-                               onChange={(e) => setUsername(e.target.value)}>
-                        </input>
-                        <label className="sign-lab" htmlFor="firstName-field">
-                            First name: * </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="firstName-field"
-                               value={firstname}
-                               onChange={(e) => setFirstname(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="lastName-field">
-                            Last name: * </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="lastName-field"
-                               value={lastname}
-                               onChange={(e) => setLastname(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="country-field">
-                            Country: * </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="country-field"
-                               value={country}
-                               onChange={(e) => setCountry(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="email-field">
-                            Email address: * </label>
-                        <input className="sign-input"
-                               type="email"
-                               id="email-field"
-                               value={email}
-                               onChange={(e) => setEmail(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="password-field">
-                            Password: * </label>
-                        <input className="sign-input"
-                               type="password"
-                               id="password-field"
-                               value={password}
-                               onChange={(e) => setPassword(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="facebook-field">
-                            Facebook: </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="facebook-field"
-                               value={facebook}
-                               onChange={(e) => setFacebook(e.target.value)}>
-                        </input>
-
-                        <label className="sign-lab" htmlFor="instagram-field">
-                            Instagram: </label>
-                        <input className="sign-input"
-                               type="text"
-                               id="instagram-field"
-                               value={instagram}
-                               onChange={(e) => setInstagram(e.target.value)}>
-                        </input>
-                        <button type="submit" className="sign-btn" disabled={loading}>
-                            {loading ? "signing up" : "Sign Up"}
-                        </button>
-                        {error && <p>{error}</p>}
-                        <p className="paragraph-form">Do you have an account? <Link className="signIn-link"to="/login">Sign In</Link>{" "} </p>
-                    </form>
-                </div>
-            </div>
-        </div>
-            );
-            }
-
+            </div>>
         </>
-    )
+    );
 }
 
-
-export default RegisterPage;
-
-
+export default Register;
