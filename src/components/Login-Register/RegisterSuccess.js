d9d2f72e13862f8af3870086ae7be7fddd48a260
marianna643 <trma0520@gmail.com>
2021-08-30 21:59:22 +0200

    add registersuccess css

---
+++ src/components/Login-Register/RegisterSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SignUpSucces.css';
+
+function RegisterSuccess () {
+    return (
+      <>
+      <div className="SignUp-message">>
+      <h1>You are successfully registered!</h1>
+      </div>
+      </>
+    )
+}
+
+export default RegisterSuccess;
